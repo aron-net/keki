b23a710c4b418b42062f3e4bbed02119812318c1
aron-helu <aronmilahela@gmail.com>
2023-01-25 06:07:26 +0300

    Adding nav-bar with link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './Components/Header/Header';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
     </div>
   );
 }
