a5a47e21eff69d05c8c28badb993d66d521a571a
aron-helu <aronmilahela@gmail.com>
2023-02-23 09:59:18 +0300

    fix eslint error

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,6 @@
 import argo from '../Image/cake-about.jpg';
 import testimonial1 from '../Image/testimonial1.jpg';
-import testimonial2 from '../Image/testimonial2.jpg'
+import testimonial2 from '../Image/testimonial2.jpg';
 import './About.css';
 
 const About = () => (
@@ -104,58 +104,66 @@ const About = () => (
       </div>
     </div>
     <div className="container-fluid py-5">
-        <div className="container">
-            <div className="section-title position-relative text-center mx-auto mb-5 pb-3" style="max-width: 600px;">
-                <h2 className="text-primary font-secondary">Testimonial</h2>
-                <h1 className="display-4 text-uppercase">Our Clients Say!!!</h1>
-            </div>
-            <div className="owl-carousel testimonial-carousel">
-                <div className="testimonial-item bg-dark text-white border-inner p-4">
-                    <div className="d-flex align-items-center mb-3">
-                        <img className="img-fluid flex-shrink-0" src={testimonial1} style="width: 60px; height: 60px;" />
-                        <div className="ps-3">
-                            <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
-                            <span>Profession</span>
-                        </div>
-                    </div>
-                    <p className="mb-0">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
-                    </p>
-                </div>
-                <div className="testimonial-item bg-dark text-white border-inner p-4">
-                    <div className="d-flex align-items-center mb-3">
-                        <img className="img-fluid flex-shrink-0" src={testimonial2} style="width: 60px; height: 60px;" />
-                        <div className="ps-3">
-                            <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
-                            <span>Profession</span>
-                        </div>
-                    </div>
-                    <p className="mb-0">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
-                    </p>
-                </div>
-                <div className="testimonial-item bg-dark text-white border-inner p-4">
-                    <div className="d-flex align-items-center mb-3">
-                        <img className="img-fluid flex-shrink-0" src="img/testimonial-3.jpg" style="width: 60px; height: 60px;" />
-                        <div className="ps-3">
-                            <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
-                            <span>Profession</span>
-                        </div>
-                    </div>
-                    <p className="mb-0">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
-                    </p>
-                </div>
-                <div className="testimonial-item bg-dark text-white border-inner p-4">
-                    <div className="d-flex align-items-center mb-3">
-                        <img className="img-fluid flex-shrink-0" src="img/testimonial-4.jpg" style="width: 60px; height: 60px;" />
-                        <div className="ps-3">
-                            <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
-                            <span>Profession</span>
-                        </div>
-                    </div>
-                    <p className="mb-0">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
-                    </p>
-                </div>
-            </div>
+      <div className="container">
+        <div className="section-title position-relative text-center mx-auto mb-5 pb-3">
+          <h2 className="text-primary font-secondary">Testimonial</h2>
+          <h1 className="display-4 text-uppercase">Our Clients Say!!!</h1>
         </div>
+        <div className="owl-carousel testimonial-carousel">
+          <div className="testimonial-item bg-dark text-white border-inner p-4">
+            <div className="d-flex align-items-center mb-3">
+              <img className="img-fluid flex-shrink-0" src={testimonial1} alt="testimonial" />
+              <div className="ps-3">
+                <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
+                <span>Profession</span>
+              </div>
+            </div>
+            <p className="mb-0">
+              Dolor et eos labore, stet justo sed est sed.
+              Diam sed sed dolor stet amet eirmod eos labore diam
+            </p>
+          </div>
+          <div className="testimonial-item bg-dark text-white border-inner p-4">
+            <div className="d-flex align-items-center mb-3">
+              <img className="img-fluid flex-shrink-0" src={testimonial2} alt="testimonial" />
+              <div className="ps-3">
+                <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
+                <span>Profession</span>
+              </div>
+            </div>
+            <p className="mb-0">
+              Dolor et eos labore, stet justo sed est sed.
+              Diam sed sed dolor stet amet eirmod eos labore diam
+            </p>
+          </div>
+          <div className="testimonial-item bg-dark text-white border-inner p-4">
+            <div className="d-flex align-items-center mb-3">
+              <img className="img-fluid flex-shrink-0" src="img/testimonial-3.jpg" alt="testimonial" />
+              <div className="ps-3">
+                <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
+                <span>Profession</span>
+              </div>
+            </div>
+            <p className="mb-0">
+              Dolor et eos labore, stet justo sed est sed.
+              Diam sed sed dolor stet amet eirmod eos labore diam
+            </p>
+          </div>
+          <div className="testimonial-item bg-dark text-white border-inner p-4">
+            <div className="d-flex align-items-center mb-3">
+              <img className="img-fluid flex-shrink-0" src="img/testimonial-4.jpg" alt="testimonial" />
+              <div className="ps-3">
+                <h4 className="text-primary text-uppercase mb-1">Client Name</h4>
+                <span>Profession</span>
+              </div>
+            </div>
+            <p className="mb-0">
+              Dolor et eos labore, stet justo sed est sed.
+              Diam sed sed dolor stet amet eirmod eos labore diam
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </>
 );
